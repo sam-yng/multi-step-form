6db53b34804d043a78b900c2cbf5a7b06a02f1af
sam-yng <young142001@hotmail.com>
2023-05-16 11:37:34 +1000

    setup

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
+import "../css/index.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
     </div>
   );
-}
+};
 
 export default App;
